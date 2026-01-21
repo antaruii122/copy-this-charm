0e35bb3f449f5c8038b7b0f99329b6d2228e76aa
Antario653 <taiwancncservices@gmail.com>
2026-01-21 14:59:18 +0800

    Remove Edit icon from inline preview card

--- src/components/admin/VideoUploadManager.tsx
+++ src/components/admin/VideoUploadManager.tsx
@@ -1774,12 +1774,7 @@ const VideoUploadManager = () => {
                                                                                     </span>
                                                                                 </div>
 
-                                                                                <div
-                                                                                    className="w-8 h-8 rounded-full flex items-center justify-center bg-black/5"
-                                                                                    style={{ backgroundColor: isWhite ? 'rgba(0,0,0,0.05)' : 'rgba(255,255,255,0.2)' }}
-                                                                                >
-                                                                                    <Edit className="w-4 h-4" style={{ color: textColor }} />
-                                                                                </div>
+                                                                                <div />
                                                                             </div>
                                                                         </>
                                                                     );
